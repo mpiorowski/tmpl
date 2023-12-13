c8427af6bb6bef2a1cec4e3fe974955a3da058e1
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-12-13 13:35:31 +0100

    update

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -16,27 +16,8 @@
 </script>
 
 <header class="bg-white shadow">
-    {#if open}
-        <Drawer position="left" title="Svelte Start" bind:open>
-            <nav class="flex flex-col space-y-4">
-                {#each navs as nav}
-                    <a
-                        href={nav.href}
-                        class="peer rounded-md px-3 py-2 text-sm font-medium focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2
-                        {$page.url.pathname === nav.href
-                            ? 'bg-indigo-100 text-indigo-700'
-                            : 'text-gray-500 hover:text-gray-700'}"
-                        aria-current={$page.url.pathname === nav.href
-                            ? "page"
-                            : undefined}
-                    >
-                        {nav.label}
-                    </a>
-                {/each}
-            </nav>
-        </Drawer>
-    {/if}
     <div class="mx-auto max-w-5xl px-2 py-4 sm:px-4 lg:px-6">
+        <!-- Mobile menu button -->
         <div class="flex items-center gap-2 sm:hidden">
             <div class="inline-flex">
                 <Button variant="link" on:click={() => (open = !open)}>
@@ -44,8 +25,29 @@
                 </Button>
             </div>
             <h1>Svelte Start</h1>
+            {#if open}
+                <Drawer position="left" title="Svelte Start" bind:open>
+                    <nav class="flex flex-col space-y-4">
+                        {#each navs as nav}
+                            <a
+                                href={nav.href}
+                                class="peer rounded-md px-3 py-2 text-sm font-medium focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2
+                        {$page.url.pathname === nav.href
+                                    ? 'bg-indigo-100 text-indigo-700'
+                                    : 'text-gray-500 hover:text-gray-700'}"
+                                aria-current={$page.url.pathname === nav.href
+                                    ? "page"
+                                    : undefined}
+                            >
+                                {nav.label}
+                            </a>
+                        {/each}
+                    </nav>
+                </Drawer>
+            {/if}
         </div>
 
+        <!-- Desktop menu -->
         <div class="hidden sm:block">
             <nav class="flex gap-x-4" aria-label="Tabs">
                 {#each navs as nav}
